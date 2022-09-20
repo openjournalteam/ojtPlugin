3a3c572dd601502392797507418f8e5e58852f3b
thisnugroho <this.nugroho@gmail.com>
2022-09-20 14:29:28 +0800

    wip

---
+++ traits/OJTPluginUpdater.inc.php
@@ -0,0 +1,17 @@
+<?php
+import('plugins.generic.ojtPlugin.helpers.OJTHelper');
+
+trait OJTPluginUpdater
+{   
+    abstract protected function getPlugin();
+
+    public function check_update()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+}
