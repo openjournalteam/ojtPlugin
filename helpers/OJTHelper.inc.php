7dfe9046b46098ff71a05ac23ea14f8b92b3784c
Rahman Ramsi <rahmanramsi19@gmail.com>
2024-03-29 10:06:19 +0800

    check when null file on log error

--- helpers/OJTHelper.inc.php
+++ helpers/OJTHelper.inc.php
@@ -85,10 +85,3 @@ if (!function_exists('vd')) {
         echo '</pre>';
     }
 }
-
-if (!function_exists('str_contains')) {
-    function str_contains($haystack, $needle)
-    {
-        return $needle !== '' && mb_strpos($haystack, $needle) !== false;
-    }
-}
