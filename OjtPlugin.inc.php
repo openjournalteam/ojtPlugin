10dac9cad5fbb2eb7eaf7737c9fcfe0aa8102323
Dede Nugroho <49790011+thisnugroho@users.noreply.github.com>
2024-04-15 11:50:57 +0800

    Update OjtPlugin.inc.php

--- OjtPlugin.inc.php
+++ OjtPlugin.inc.php
@@ -357,9 +357,12 @@ class OjtPlugin extends GenericPlugin
 
     public function getDefaultPluginIcon()
     {
-        $templateMgr = TemplateManager::getManager($this->getRequest());
-
-        return $templateMgr->fetch($this->getTemplateResource('defaultIcon.tpl'));
+        // In some ojs this func trigger error, can't read defaultIcon.tpl
+        // $templateMgr = TemplateManager::getManager($this->getRequest());
+        // return $templateMgr->fetch($this->getTemplateResource('defaultIcon.tpl'));
+        return '<svg class="ojt-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
+d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
+</svg>';
     }
 
     public function createModulesFolder()
