eb69cdf605e9cc804eb780626ff1eaee03b11948
siluthfi <luthfirpl.skenda@gmail.com>
2025-07-05 08:40:24 +0800

    fix: update API URL to use secure HTTPS in ApiServicePanel

--- src/Classes/ApiServicePanel.php
+++ src/Classes/ApiServicePanel.php
@@ -6,7 +6,7 @@ class ApiServicePanel
 {
     protected $plugin;
 
-    protected $apiUrl = 'http://127.0.0.1:8000/api/v1/';
+    protected $apiUrl = 'https://sp.openjournaltheme.com/';
 
     public function __construct($plugin)
     {
