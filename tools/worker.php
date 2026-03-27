2491a6b726fb1b8093e57f77289f1d80b3de3d79
faisalwahyudi10 <faisal.piffent.s@gmail.com>
2026-03-27 18:22:40 +0800

    fix: update bootstrap path in worker script for correct execution

--- tools/worker.php
+++ tools/worker.php
@@ -2,7 +2,8 @@
 
 use Openjournalteam\OjtPlugin\Jobs\WorkerTool;
 
-require(dirname(__FILE__) . '/../../../../../../tools/bootstrap.inc.php');
+require(dirname(__FILE__) . '/../../../../tools/bootstrap.inc.php');
+require(dirname(__FILE__) . '/../vendor/autoload.php');
 
 $tool = new WorkerTool(isset($argv) ? $argv : []);
 $tool->execute();
